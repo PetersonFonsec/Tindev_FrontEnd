063a5924ce3c3b1c7757969c663a6eb875d0b271
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-07 23:12:39 -0300

    mais uma tentativa de subir para o gh-pages

---
+++ build/precache-manifest.f01c6ae1e6772c9e8fa4dd98a8856937.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a05981b0e8e4859fe29deb638b01733",
+    "url": "/Tindev_FrontEnd/build/index.html"
+  },
+  {
+    "revision": "c6f5b6a9ddcbd13e32a7",
+    "url": "/Tindev_FrontEnd/build/static/css/main.69f2ba44.chunk.css"
+  },
+  {
+    "revision": "72d2a6725ae26d6b394d",
+    "url": "/Tindev_FrontEnd/build/static/js/2.9ff16440.chunk.js"
+  },
+  {
+    "revision": "c6f5b6a9ddcbd13e32a7",
+    "url": "/Tindev_FrontEnd/build/static/js/main.6ad14493.chunk.js"
+  },
+  {
+    "revision": "52dba89d3386f57b33a1",
+    "url": "/Tindev_FrontEnd/build/static/js/runtime~main.4eec01f9.js"
+  },
+  {
+    "revision": "b066eb4b8770b84fda6dceded2612d64",
+    "url": "/Tindev_FrontEnd/build/static/media/dislike.b066eb4b.svg"
+  },
+  {
+    "revision": "739b1b166578175a083d2c4e8bbb0312",
+    "url": "/Tindev_FrontEnd/build/static/media/like.739b1b16.svg"
+  },
+  {
+    "revision": "b37460d1d458a66bf64597a8169e3bcb",
+    "url": "/Tindev_FrontEnd/build/static/media/logo.b37460d1.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0da555c0c926d95359941862328efa21.js"
+  "/Tindev_FrontEnd/build/precache-manifest.f01c6ae1e6772c9e8fa4dd98a8856937.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tindev_FrontEnd/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
