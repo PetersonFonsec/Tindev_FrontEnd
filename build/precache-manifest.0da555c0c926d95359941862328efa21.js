43cfc7bf87079c43f00eda3a6893bfafd9b8824e
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-07 23:07:13 -0300

    mais uma tentativa de subir para o gh-pages

---
+++ build/precache-manifest.0da555c0c926d95359941862328efa21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ceca33fca9b8951c19b2ee71424969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f5b6a9ddcbd13e32a7",
+    "url": "/static/css/main.69f2ba44.chunk.css"
+  },
+  {
+    "revision": "72d2a6725ae26d6b394d",
+    "url": "/static/js/2.9ff16440.chunk.js"
+  },
+  {
+    "revision": "c6f5b6a9ddcbd13e32a7",
+    "url": "/static/js/main.6ad14493.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b066eb4b8770b84fda6dceded2612d64",
+    "url": "/static/media/dislike.b066eb4b.svg"
+  },
+  {
+    "revision": "739b1b166578175a083d2c4e8bbb0312",
+    "url": "/static/media/like.739b1b16.svg"
+  },
+  {
+    "revision": "b37460d1d458a66bf64597a8169e3bcb",
+    "url": "/static/media/logo.b37460d1.svg"
+  }
+]);
